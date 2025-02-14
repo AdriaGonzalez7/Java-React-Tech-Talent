ecef68d900f901d7c2c9336654a35e6375a87d27
AdriaGonzalez7 <adriagonzalez777@gmail.com>
2025-02-14 10:26:33 +0100

    cambios y c2

---
+++ C2/src/PruebaApp.java
@@ -0,0 +1,13 @@
+
+public class PruebaApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 30; // Declaro el tipo y el nombre de la variable y luego le asigno un valor 
+		double b = 30.5; // Declaro tipo nombre y asigno valor
+		a = 50;
+//		int a = 32; No puedes porque se esta declarando de nuevo la misma variable
+		double suma = a + b;
+		System.out.println(suma); // Escribes syso Ctrl + espacio
+	}
+}
