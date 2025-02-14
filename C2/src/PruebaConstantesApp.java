e35ef07f889039fcd4e3e0de406cb715daccc77c
AdriaGonzalez7 <adriagonzalez777@gmail.com>
2025-02-14 11:02:59 +0100

    constantes

---
+++ C2/src/PruebaConstantesApp.java
@@ -0,0 +1,13 @@
+
+public class PruebaConstantesApp {
+
+	public static void main(String[] args) {
+		final double IVA = 0.18;
+		int producto = 300;
+		
+		double resultado = producto*IVA;
+		System.out.println("El Iva del producto es:" + resultado);
+
+	}
+
+}
