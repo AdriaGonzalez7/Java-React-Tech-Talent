152cbf24d171e9d13754b59a268217dc328168fc
AdriaGonzalez7 <adriagonzalez777@gmail.com>
2025-02-14 13:58:00 +0100

    cambios ejercicio4

---
+++ C2/src/Tarea4Ejercicio1.java
@@ -0,0 +1,17 @@
+
+public class Tarea4Ejercicio1 {
+
+	public static void main(String[] args) {
+		int num1 = 3;
+		int num2 = 2;
+		System.out.println("La suma es:"+ (num1 + num2));
+		System.out.println("La resta es:"+ (num1 - num2));
+		System.out.println("La multiplicacion es:"+ (num1 * num2));
+		System.out.println("La divison es:"+ (num1 / num2));
+		System.out.println("El modulo (resto de la division es):"+ (num1 % num2));
+
+
+
+	}
+
+}
---
+++ C2/src/Tarea4Ejercicio2.java
@@ -0,0 +1,17 @@
+
+public class Tarea4Ejercicio2 {
+
+	public static void main(String[] args) {
+		int N = 1;
+		double A = 2;
+		char C = 'A';
+		
+		System.out.printf("Variable N= "+ N +"%nVariable A= "+ A + "%nVariable C= " + C +"%n" );
+		System.out.println( N + " + " + A + " = " + (N + A));
+		System.out.printf( N + " - " + A + " = " + (A - N));
+
+//		System.out.println("La suma es:"+ (num1 + num2));
+
+	}
+
+}
