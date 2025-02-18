5bcb749f5e86d06f7eed3a9c971200be896b5465
AdriaGonzalez7 <adriagonzalez777@gmail.com>
2025-02-18 13:56:46 +0100

    Ejercicios 1-9

--- C2/src/Tarea5/Tarea5Ejercicio8.java
+++ C2/src/Tarea5/Tarea5Ejercicio8.java
@@ -1,13 +1,11 @@
 package Tarea5;
 
-public class Tarea5Ejercicio7 {
+public class Tarea5Ejercicio8 {
 
 	public static void main(String[] args) {
-		int num = 1;
-		while(num<101) {
+		for (int num = 1; num < 101; num++){
 			System.out.println(num);
-			num++;
 		}
-	}
 
+	}
 }
---
+++ C2/src/Tarea5/Tarea5Ejercicio9.java
@@ -0,0 +1,14 @@
+package Tarea5;
+
+public class Tarea5Ejercicio9 {
+
+	public static void main(String[] args) {
+		for(double num = 1; num < 101; num++) {
+		while ((num%2 == 0)||(num%3 == 0)){
+			System.out.println(num);
+			num++;
+			}
+		}
+	}
+	
+}
