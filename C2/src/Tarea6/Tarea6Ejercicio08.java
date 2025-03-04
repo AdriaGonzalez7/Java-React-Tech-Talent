a5dc149be5bc9bfadffe57e0f792af9efeabbd7c
AdriaGonzalez7 <adriagonzalez777@gmail.com>
2025-03-04 13:42:54 +0100

    t6

--- C2/src/Tarea6/Tarea6Ejercicio08.java
+++ C2/src/Tarea6/Tarea6Ejercicio08.java
@@ -5,21 +5,22 @@ import java.util.Scanner;
 public class Tarea6Ejercicio08 {
 
 	public static void main(String[] args) {
-	RellenarArray(array[], lista);
-	ImprimirArray(new int[10]);
+	int lista[]= RellenarArray(10);
+	ImprimirArray(lista);
 	}
-	public static int[] RellenarArray(int array[]) {
+	public static int[] RellenarArray(int a) {
 		int lista[] = new int[10];
 		System.out.println("Introduce 10 números enteros: ");
 		for (int i = 0; i < lista.length; i++) {
 			Scanner sc = new Scanner(System.in);
 			lista[i] = Integer.parseInt(sc.nextLine());
 		}
-		return lista;
+		return	lista;
 	}
 	public static void ImprimirArray (int array[]) {
 		for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            System.out.println("Inidice: " + i);
+            System.out.println(array[i]);	
         }
 	
 	}
---
+++ C2/src/Tarea6/Tarea6Ejercicio09.java
@@ -0,0 +1,37 @@
+package Tarea6;
+
+import java.util.Scanner;
+
+public class Tarea6Ejercicio09 {
+
+	public static void main(String[] args) {
+		int lista[]= RellenarArray();
+		ImprimirArray(lista);
+		}
+	public static int [] RellenarArray () {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce el tamaño del array: ");
+		int rango = Integer.parseInt(sc.nextLine());
+		int lista[]	= new int[rango];
+		// Rellenar array con numeros aleatorios
+			for (int i = 0; i < lista.length; i++) {
+				lista[i] = (int) (Math.random() * 10);	
+			}
+		sc.close();
+		return lista;
+	}
+		
+	public static void ImprimirArray (int array []) {
+		int suma = 0; 
+		for (int i = 0; i < array.length; i++) {
+        System.out.println("Índice " + i + ": " + array[i]);
+        suma += array[i];
+	}
+		System.out.println("La suma de los elementos del array es: " + suma);
+	}
+	
+	
+	
+	
+	}
+
