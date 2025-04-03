4df328d3ca59c846c710384d0ae900b3f47e6900
AdriaGonzalez7 <adriagonzalez777@gmail.com>
2025-04-03 10:58:54 +0200

    progress t9 ex1

--- C2/src/Tarea9/Lavadora.java
+++ C2/src/Tarea9/Lavadora.java
@@ -1,5 +1,41 @@
 package Tarea9;
 
-public class Lavadora {
+public class Lavadora extends Electrodomestico {
+	// Atributos
+	private int carga;
+	// Constantes
+	private static final int carga_por_defecto = 5;
 
+	// Constructor por defecto
+	public Lavadora() {
+		super();
+		this.carga = carga_por_defecto;
+	}
+
+	// Un constructor con el precio y peso. El resto por defecto.
+	
+	public Lavadora(double precio_base, int peso) {
+		super(precio_base, peso);
+		this.carga = carga_por_defecto;
+	}
+	
+	// Constructor con la carga y el resto atributos heredados
+	
+	public Lavadora(int carga, double precio_base, String color, char consumo_energetico, int peso) {
+		super(color, consumo_energetico, precio_base, peso);
+		this.carga = carga;
+	}
+	// Getters
+	public int getCarga() {
+		return carga;
+	}
+
+	// Metodo precioFinal
+	public double precioFinal() {
+		double precio_final = super.precioFinal();
+		if (carga > 30) {
+			precio_final += 50;
+		}
+		return precio_final;
+	}
 }
--- C2/src/Tarea9/Television.java
+++ C2/src/Tarea9/Television.java
@@ -1,5 +1,53 @@
 package Tarea9;
 
-public class Television {
-
+public class Television extends Electrodomestico {
+	// Atributos
+	private int resolucion;
+	private boolean sintonizador_TDT;
+	// Constantes
+	private static final int resolucion_por_defecto = 20;
+	private static final boolean sintonizador_TDT_por_defecto = false;
+	
+	// Constructor por defecto
+	public Television() {
+		super();
+		this.resolucion= resolucion_por_defecto;
+		this.sintonizador_TDT = sintonizador_TDT_por_defecto;	
+	}
+	
+	// Constructor con el precio y el peso, el resto por defecto
+	public Television(double precio_base, int peso) {
+		super(precio_base, peso);
+		this.resolucion = resolucion_por_defecto;
+		this.sintonizador_TDT = sintonizador_TDT_por_defecto;
+	}
+	
+//	Un constructor con la resolución, sintonizador TDT y el resto de atributos heredados. 
+	public Television(int resolucion, boolean sintonizador_TDT, double precio_base, String color,
+			char consumo_energetico, int peso) {
+		super(color, consumo_energetico, precio_base, peso);
+		this.resolucion = resolucion;
+		this.sintonizador_TDT = sintonizador_TDT;
+	}
+	
+	// Getters
+	public int getResolucion() {
+		return resolucion;
+	}
+	public boolean getSintonizador_TDT() {
+		return sintonizador_TDT;
+	}
+	
+	// Metodo precioFinal
+		public double precioFinal() {
+			double precio_final = super.precioFinal();
+			if (resolucion > 40) {
+				precio_final = precio_final * 1.3;
+			}
+			if (sintonizador_TDT) {
+				precio_final += 50;
+			}
+			return precio_final;
+		}
+	
 }
