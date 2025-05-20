22e73a4cbd03670b91f8afdd178b3758e91a7295
AdriaGonzalez7 <adriagonzalez777@gmail.com>
2025-05-20 19:55:40 +0200

    Update Ejercicio4.sql

--- C3/Tarea16/Ejercicio4.sql
+++ C3/Tarea16/Ejercicio4.sql
@@ -54,8 +54,20 @@ SELECT * FROM Salas WHERE Pelicula IS NULL;
 SELECT Salas.*, Peliculas.* FROM Salas LEFT JOIN Peliculas ON Salas.Pelicula = Peliculas.codigo;
 
 
+-- 4.6. Mostrar la información de todas las películas y, si se proyecta en alguna sala, mostrar también la información de la sala.
+SELECT Peliculas.*, Salas.* FROM Peliculas LEFT JOIN Salas ON Peliculas.codigo = Salas.Pelicula;
 
+-- 4.7. Mostrar los nombres de las películas que no se proyectan en ninguna sala.
+SELECT Nombre FROM Peliculas WHERE codigo NOT IN (SELECT Pelicula FROM Salas);
 
+-- 4.8. Añadir una nueva película 'Uno, Dos, Tres', para mayores de 7 años.
+INSERT INTO Peliculas (codigo, Nombre, CalificacionEdad) VALUES (11, 'Uno, Dos, Tres', 7);
+
+-- 4.9. Hacer constar que todas las películas no calificadas han sido calificadas 'no recomendables para menores de 13 años'.
+UPDATE Peliculas SET CalificacionEdad = 13 WHERE CalificacionEdad IS NULL;
+
+-- 4.10. Eliminar todas las salas que proyectan películas recomendadas para todos los públicos.
+DELETE FROM Salas WHERE Pelicula IN (SELECT codigo FROM Peliculas WHERE CalificacionEdad = 13);
 
 
 
