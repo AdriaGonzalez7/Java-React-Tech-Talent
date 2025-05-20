28df61075e4a7b93b2d0622b4b14733385ec7f19
AdriaGonzalez7 <adriagonzalez777@gmail.com>
2025-05-20 12:28:51 +0200

    aaa

---
+++ C2/src/CalculadaoraEquipo/Calculadora.java
@@ -0,0 +1,13 @@
+package CalculadaoraEquipo;
+
+import javax.swing.*;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        // Crear la ventana principal
+        SwingUtilities.invokeLater(() -> {
+            VentanaCalculadora ventana = new VentanaCalculadora();
+            ventana.setVisible(true);
+        });
+    }
+}
