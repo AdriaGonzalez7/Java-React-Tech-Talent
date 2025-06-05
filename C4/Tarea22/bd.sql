e68bf4e82873847dfa18b495b29320e72780217c
AdriaGonzalez7 <adriagonzalez777@gmail.com>
2025-06-05 13:12:52 +0200

    Create bd.sql

---
+++ C4/Tarea22/bd.sql
@@ -0,0 +1,23 @@
+create database bd_tarea22;
+
+use bd_tarea22;
+
+create table clientes(
+    id int primary key NOT NULL AUTO_INCREMENT,
+    nombre varchar(50) DEFAULT NULL,
+    apellido varchar(50) DEFAULT NULL,
+    direccion varchar(50) DEFAULT NULL,
+    dni varchar(9) DEFAULT NULL,
+    fecha date DEFAULT NULL,
+    PRIMARY KEY (id)
+);
+
+create table videos (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    title varchar(250) DEFAULT NULL,
+    director varchar(250) DEFAULT NULL,
+    cli_id int(11) DEFAULT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT videos_fk FOREIGN KEY (cli_id) REFERENCES clientes(id)
+);
+
